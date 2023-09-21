f3f864950830482434178edeea2bcff6bc176bf5
Marjorie Wuckert <mwuckert@intra.42.fr>
2023-09-21 10:36:29 +0300

    modify moulitest_pgroups.sh

--- piscine/day01/ex01/moulitest_printgroups.sh
+++ piscine/day01/ex01/moulitest_printgroups.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 export FT_USER="daemon"
+echo "[$FT_USER]:"
 ./print_groups.sh
 
 export FT_USER="root"
+echo "[$FT_USER]:"
 ./print_groups.sh
 
 export FT_USER="macushka"
+echo "[$FT_USER]:"
 ./print_groups.sh
